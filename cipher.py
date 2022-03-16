e8a552aa3a274066b26628076f0dffceb37aaa8e
Quantum0109 <72807872+Quantum0109@users.noreply.github.com>
2022-03-16 16:54:26 +0300

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("encrypt&bruteforce")
+
+main_frame = tk.Frame(window, padx=20, pady=20)
+
+alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+alphabet = alphabet + alphabet.upper()
+
+#слово которое нежно зашифровать
+message = "слово"
+
+def encrypt(message: str, shift: int) -> str:
+	result = ""
+	for i in message:
+		if i in alphabet:
+			index = alphabet.index(i)
+			index_shifted = (index + shift) % len(alphabet)
+			result += alphabet[index_shifted]
+		else: 
+			result += i
+	return result
+message_encrypted = encrypt(message, 1)
+def deciphement(message_encrypted: str, func) -> str:
+	for i in range(len(alphabet)):
+		print(i, encrypt(message_encrypted, i * -1))
+
+print(encrypt(message, 1))
+deciphement(message_encrypted, encrypt)
+"""
+    TODO:
+    	Добавить tkinter
+    	Убрать cmd 
+    	Сделать .exe файл
+"""
+main_frame.mainloop()
